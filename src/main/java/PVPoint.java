9156af3c1f8a1e82cdc15eca3bf532c268a5bde5
XenoProxy <ksenomorph6@gmail.com>
2020-07-09 15:13:45 +0300

    Points with 12 coordinates were added. Bugs were fixed

--- src/main/java/PVPoint.java
+++ src/main/java/PVPoint.java
@@ -7,19 +7,19 @@ import java.util.List;
 
 class PVPoint{
     double[] position = new double[6];
-    //double[] velocity = new double[6];
+    double[] velocity = new double[6];
 
-    public PVPoint(double[] position)
+    public PVPoint(double[] position, double[] velocity)
     {
         for(int i = 0; i < position.length; i++){
             this.position[i] = position[i];
-            //this.velocity[i] = velocity[i];
+            this.velocity[i] = velocity[i];
         }
     }
 
     @Override
     public String toString(){
-        return "positions:" + position; //+ "/n velocities:" + velocity;
+        return "positions:" + position + "/n velocities:" + velocity;
     }
 }
 
@@ -47,9 +47,8 @@ class ScaleService{
         this.listLength = listLength;
     }
 
-    public String getInputPoints(){
-        String stringInputArray = inputPoints.toString();
-        return stringInputArray;
+    public List<PVPoint> getInputPoints(){
+        return inputPoints;
     }
 
     public int getListLength(){
@@ -58,24 +57,25 @@ class ScaleService{
 
     public List<PVPoint> createPointList(){
         List<PVPoint> listPoint = new ArrayList<PVPoint>();
-        listPoint.add(new PVPoint(new double[] {0, 100, 50, 0, 0, 0}));
-        listPoint.add(new PVPoint(new double[] {100, 300, 100, 50, 100, 100}));
-        listPoint.add(new PVPoint(new double[] {200, 500, 150, 100, 200, 200}));
-        listPoint.add(new PVPoint(new double[] {300, 700, 200, 150, 300, 300}));
-        listPoint.add(new PVPoint(new double[] {400, 900, 250, 200, 400, 400}));
-        listPoint.add(new PVPoint(new double[] {500, 1200, 300, 250, 500, 600}));
+        listPoint.add(new PVPoint(new double[] {0, 100, 50, 0, 0, 0}, new double[] {0, 10, 10, 3, 4, 5}));
+        listPoint.add(new PVPoint(new double[] {100, 300, 100, 50, 100, 100}, new double[] {5, 20, 15, 50, 100, 100}));
+        listPoint.add(new PVPoint(new double[] {200, 500, 150, 100, 200, 200}, new double[] {10, 30, 20, 50, 100, 100}));
+        listPoint.add(new PVPoint(new double[] {300, 700, 200, 150, 300, 300}, new double[] {15, 40, 25, 50, 100, 100}));
+        listPoint.add(new PVPoint(new double[] {400, 900, 250, 200, 400, 400}, new double[] {20, 50, 30, 50, 100, 100}));
+        listPoint.add(new PVPoint(new double[] {500, 1200, 300, 250, 500, 600}, new double[] {25, 60, 35, 50, 100, 100}));
         return  listPoint;
     }
 
 //Поочередная обработка переменных точек из списка
-    public String scaleService(double speed, int index) {
+    public String scaleService(double speed, int index, double[] input) {
         String[] resultArray = new String[6];
         List<Double> scaleSpeed = new ArrayList<Double>();
-        scaleSpeed.add(0, inputPoints.get(0).position[index]); // установка начальной точки
+        scaleSpeed.add(0, input[0]); // установка начальной точки
         double percentSpeed = speed / 100;
-        int dP = (int) ((inputPoints.get(1).position[index] - inputPoints.get(0).position[index])); // шаг
+        int dP = (int) ((input[1] - input[0])); // шаг
         double newSize = Math.ceil(listLength * 100 / speed);
-        double endPoint = inputPoints.get(inputPoints.size()-1).position[index]; // установка последней точки
+        double endPoint = input[input.length-1]; // установка последней точки
+
         for (int i = 1; i < newSize; i++) {
             double inputScale = scaleSpeed.get(i - 1) + dP * percentSpeed;
             if (inputScale <= endPoint) {
@@ -100,14 +100,17 @@ class Program {
         SpeedPlan speed = new SpeedPlan();
         int input = Integer.parseInt(in.readLine());
         speed.setSpeed(input);
-        List<Double> inPoint = new ArrayList<Double>(); //список координат точек
+        double[] inPositions = new double[6]; //список углов моторов в точках
+        double[] inVelocities = new double[6]; //список скоростей моторов в точках
         List<PVPoint> listPoint = scale.createPointList(); //переписали сюда создавшийся список точек
         for(int j = 0; j < scale.getListLength(); j++) { // j отвечает за номер мотора
             for(int pj = 0; pj < scale.getListLength(); pj++) { // pj отвечает за номер точки
-                inPoint.add(pj, listPoint.get(pj).position[j]);
+                inPositions[pj] = listPoint.get(pj).position[j];
+                inVelocities[pj] = listPoint.get(pj).velocity[j];
                 scale.setInputPoints(listPoint); // записываем входные точки
             }
-            System.out.println(scale.scaleService(speed.getSpeed(), j));
+            System.out.println("Positions: " + scale.scaleService(speed.getSpeed(), j, inPositions));
+            System.out.println("Velocities: " + scale.scaleService(speed.getSpeed(), j, inVelocities) + "\n");
         }
     }
 }
