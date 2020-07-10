b3fb7850069c038761247867a0db9c8fd6e07d28
XenoProxy <ksenomorph6@gmail.com>
2020-07-10 17:26:05 +0300

    The total sum of all points was added

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -11,7 +11,9 @@ public class Program {
         scale.setListLength(scale.createPointList().size());
         SpeedPlan speed = new SpeedPlan();
         List<PVPoint> listOriginPoint = scale.createPointList(); //сюда переписали начальный список точек
-        int allPoints = listOriginPoint.size();
+        int allPoints = 0; //всего точек в списке
+        int sumOfPoints = 0; //итоговая сумма точек, полученных после вычислений
+        while (allPoints < listOriginPoint.size()) {
 
         System.out.println("Enter a percent of points: ");
         int inputPercent = Integer.parseInt(in.readLine());
@@ -21,19 +23,24 @@ public class Program {
         int inputSpeed = Integer.parseInt(in.readLine());
         speed.setSpeed(inputSpeed);
 
-        double[] inPositions = new double[pointsSet]; //список углов моторов в точках
-        double[] inVelocities = new double[pointsSet]; //список скоростей моторов в точках
-        List<PVPoint> pointsSetList = pointsSet(pointsSet, listOriginPoint);
-        for(int j = 0; j < 6; j++) { // j отвечает за номер мотора
-            for(int pj = 0; pj < pointsSet; pj++) { // pj отвечает за номер точки
-                inPositions[pj] = pointsSetList.get(pj).position[j];
-                inVelocities[pj] = pointsSetList.get(pj).velocity[j];
-                scale.setInputPoints(pointsSetList); // записываем входные точки
-            }
+            double[] inPositions = new double[pointsSet]; //список углов моторов в точках
+            double[] inVelocities = new double[pointsSet]; //список скоростей моторов в точках
+            List<PVPoint> pointsSetList = pointsSet(pointsSet, listOriginPoint);
+            for (int j = 0; j < 6; j++) { // j отвечает за номер мотора
+                for (int pj = 0; pj < pointsSet; pj++) { // pj отвечает за номер точки
+                    inPositions[pj] = pointsSetList.get(pj).position[j];
+                    inVelocities[pj] = pointsSetList.get(pj).velocity[j];
+                    scale.setInputPoints(pointsSetList); // записываем входные точки
+                }
             System.out.println("Motor's positions: " + scale.scaleService(speed.getSpeed(), j, inPositions));
             System.out.println("Motor's velocities: " + scale.scaleService(speed.getSpeed(), j, inVelocities) + "\n");
+            }
+            sumOfPoints += pointsSet * 100/ speed.getSpeed();
+            System.out.println(sumOfPoints);
+            allPoints += pointsSet;
         }
-        //System.out.println(pointsSetList);
+        System.out.println("All points: " + allPoints);
+        System.out.println("Sum of points: " + sumOfPoints);
     }
 
     public static List<PVPoint> pointsSet(int pointsSet, List<PVPoint> originList){ //вычисляет набор точек для обработки
